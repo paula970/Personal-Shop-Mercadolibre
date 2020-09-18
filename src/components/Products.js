408a82b8af383a59c5960ddd366d24f8c225a2c3
paula970 <andrea.maldonado@udea.edu.co>
2020-09-18 10:21:59 -0500

    version 2

--- src/components/Products.js
+++ src/components/Products.js
@@ -10,6 +10,7 @@ const Products = (props) => {
     // se lleva al componente principal 
     //this.props.vendedorID(b);
     //props.Products.vendedorID(b);
+    //props.Products.vendedorID(b);
     return (
         <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
             <div className="card">
