1bea203b7b97e7446cd0b11706a2a7a92195cd78
paula970 <andrea.maldonado@udea.edu.co>
2020-10-13 21:44:46 -0500

    version final

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom'
 import Page1 from './pages/Page1'
 import Page2 from './pages/Page2'
 
@@ -9,7 +9,11 @@ const App = () => (
     <BrowserRouter>
         <Switch>
             <Route exact path="/productos" component={Page1} />
-            <Route path="/productos/:id" name="productos" component={Page2} />       
+            
+            <Route path="/productos/:id" name="productos" component={Page2} /> 
+            <Route component={Page1}>
+                <Redirect to = "/productos"></Redirect>
+            </Route>      
         </Switch>
     </BrowserRouter>
 )
